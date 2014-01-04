6a2f8c9becbf5212fb92ace8a9d7add9c4e0ad52
Malachi, Brendon <brendonmalachi@gmail.com>
2014-01-04 23:31:32 +0200

    Update

--- IRRxTester.py
+++ IRRxTester.py
@@ -23,6 +23,14 @@ import pdb
 #Local var's
 RowLen=56
 RowSpace=2
+# IR Protocol parmater
+AGC_BURST_TIME=9
+AGC_RELEX_TIME=4.5
+CARIRER_TIME=0.56
+LOGIC_HIGH=2.25
+LOGIC_LOW=1.12
+END_MARKER=39.9
+REPET_MARKER=96.6
 
 try:
     import GPIOWrapperClass as GPIO
@@ -43,9 +51,13 @@ if __name__=='__main__':
     tick=0
     tock=0
     Counter=0
+    IRList=[]
     
     def cbRedButtonPress(Ch):
-        print 'Red Button got pressed'
+        if Ch==99:
+            pass
+        else:
+            print 'Red Button got pressed'
         global tick
         global tock
         global Counter
@@ -53,31 +65,62 @@ if __name__=='__main__':
         tick=0
         tock=0
         Counter=0
+        IRList=[]
         
     def cbYellowButtonPress(Ch):
         print 'Yellow Button got pressed'
 
+    def DetectLastMarker():
+        global IRList
+        global Counter
+        if len(IRList)>=4:
+            #print '[-1]=%f, [-2]=%f, [-3]=%f, [-4]=%f' %(IRList[-1], IRList[-2], IRList[-3], IRList[-4])
+            #print '[-1] %f-%f , [-2] %f-%f, [-3] %f-%f, [-4] %f-%f' %(CARIRER_TIME*0.75, CARIRER_TIME*1.25, LOGIC_HIGH*0.85, LOGIC_HIGH*1.15, AGC_BURST_TIME*0.85, AGC_BURST_TIME*1.15, END_MARKER*0.85, END_MARKER*1.15)
+            #print '[-1] %f-%f , [-2] %f-%f, [-3] %f-%f, [-4] %f-%f' %(CARIRER_TIME*0.75, CARIRER_TIME*1.25, LOGIC_HIGH*0.85, LOGIC_HIGH*1.15, AGC_BURST_TIME*0.85, AGC_BURST_TIME*1.15, REPET_MARKER*0.85, REPET_MARKER*1.15)            
+            # Single Press end
+            if IRList[-1]>=CARIRER_TIME*0.75 and IRList[-1]<=CARIRER_TIME*1.25:
+                #print 'pass -1, [-1]=%f, [-2]=%f, [-3]=%f, [-4]=%f' %(IRList[-1], IRList[-2], IRList[-3], IRList[-4])
+                if IRList[-2]>=LOGIC_HIGH*0.85 and IRList[-2]<=LOGIC_HIGH*1.15:
+                    #print 'pass -2, [-1]=%f, [-2]=%f, [-3]=%f, [-4]=%f' %(IRList[-1], IRList[-2], IRList[-3], IRList[-4])
+                    if IRList[-3]>=AGC_BURST_TIME*0.85 and IRList[-3]<=AGC_BURST_TIME*1.15:                       
+                        #print 'pass -3, [-1]=%f, [-2]=%f, [-3]=%f, [-4]=%f' %(IRList[-1], IRList[-2], IRList[-3], IRList[-4])
+                        if IRList[-4]>=END_MARKER*0.85 and IRList[-4]<=END_MARKER*1.15:
+                            print 'Singel press'
+                            Counter=0
+                            IRList=[IRList[-1]]
+                            #cbRedButtonPress(99)
+                        elif IRList[-4]>=REPET_MARKER*0.85 and IRList[-4]<=REPET_MARKER*1.15:
+                            print 'Repeat press'
+                            Counter=0
+                            IRList=[IRList[-1]]                           
+                            #cbRedButtonPress(99)
+                        else: print 'did not detect nothing'
+            
+        
     def cbIRToggle(Ch):
         #http://www.sbprojects.com/knowledge/ir/nec.php
         global tick
         global tock
         global Counter
+        global IRList
         tock = time()
 ##        tock = clock()                
         Counter+=1
         Diff = (tock-tick)*1e3
-        if Diff>30:
-            Val='End'
-        elif Diff>7:
-            Val ='AGC'
-        elif Diff>3.5:
-            Val='Space'
-        elif Diff>1 and Diff<2:
-            Val=1
-        else:
-            Val=-9999
-        print 'IR toogle #%d, time Diff=%f [mSec], Bit=%d' %(Counter, Diff, Val)
+        IRList.append(Diff)
+##        if Diff>30:
+##            Val='End'
+##        elif Diff>7:
+##            Val ='AGC'
+##        elif Diff>3.5:
+##            Val='Space'
+##        elif Diff>1 and Diff<2:
+##            Val=1
+##        else:
+##            Val=-9999
+        print 'IR toogle #%d, time Diff=%f [mSec]' %(Counter, Diff)
         tick = tock
+        DetectLastMarker()
         
     pin_mode='BOARD'
     warning_level=False
